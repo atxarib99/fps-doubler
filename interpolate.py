06c75e4deb885c9080452df58f068a130e73db8c
Arib Dhuka <atxarib99@gmail.com>
2022-02-17 01:43:00 -0600

    using integer division for interpolation. added calc vs target frame output with tkinter

--- interpolate.py
+++ interpolate.py
@@ -4,25 +4,18 @@ from tracemalloc import start
 import cv2
 import numpy as np
 import time
-from PIL import Image
-#import tkinter as tk
+from PIL import Image, ImageTk
+import tkinter as tk
 
-video = cv2.VideoCapture('/mnt/d/Splitgate Highlights/RAWs/ATLANTIS RACE.mp4')
+debug = True
+
+video = cv2.VideoCapture('STADIUM RACE.mp4')
 length = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
 
 success = True
 
 #tkinter stuff
-# img = Image.fromarray(im_arr)
-
-# root = tk.Tk()
-# canvas = tk.Canvas(root)
-# canvas.pack()
-
-# imgtk = ImageTk.PhotoImage(img)
-
-# canvas.create_image(image=imgtk)
-
+root = tk.Tk()
 
 #holds the max/min error found
 max_err = 0
@@ -36,16 +29,24 @@ min_frame_target = None
 
 
 def saveFrame(calc_frame, target_frame, minormax):
+    global max_frame_calc
+    global min_frame_calc
+    global max_frame_target
+    global min_frame_target
     if minormax == 'max':
         max_frame_calc = calc_frame
         max_frame_target = target_frame
-
+    else:
+        min_frame_calc = calc_frame
+        min_frame_target = target_frame
 
 
 final_err = 0
 count = 0
 starttime = 0
 while success:
+    if debug and count > 1000:
+        break
     starttime = time.time()
     #grab primary frame
     success, im_arr = video.read()
@@ -62,7 +63,7 @@ while success:
     #         for p in range(0, len(im_arr[r][c])):
     #             calc_im_arr[r][c][p] = (int(im_arr[r][c][p]) + int(next_im_arr[r][c][p])) / 2
     
-    calc_im_arr = (im_arr + next_im_arr) / 2
+    calc_im_arr = (im_arr + next_im_arr) // 2
     
     #calc err
     # for r in range(0, len(im_arr)):
@@ -89,3 +90,51 @@ final_err /= count
 
 print('FINAL ERROR: ', final_err)
 
+max_calc_canvas = tk.Canvas(root, width=1920, height=1080)
+max_calc_canvas.pack()
+
+max_frame_calc = max_frame_calc[...,[2,1,0]]
+img = Image.fromarray(max_frame_calc)
+imgtk = ImageTk.PhotoImage(img)
+max_calc_canvas.create_image(0, 0, anchor=tk.NW, image = imgtk)
+max_calc_canvas.create_text(100,10,text="max_frame_calc")
+max_calc_canvas.pack()
+
+root2 = tk.Toplevel(root)
+max_target_canvas = tk.Canvas(root2, width=1920, height=1080)
+max_target_canvas.pack()
+
+img2 = Image.fromarray(max_frame_target)
+imgtk2 = ImageTk.PhotoImage(img2)
+max_target_canvas.create_image(0, 0, anchor=tk.NW, image = imgtk2)
+max_target_canvas.create_text(100,10,text="max_frame_target")
+max_target_canvas.pack()
+
+root3 = tk.Toplevel(root)
+min_calc_canvas = tk.Canvas(root3, width=1920, height=1080)
+min_calc_canvas.pack()
+
+img3 = Image.fromarray(min_frame_calc)
+imgtk3 = ImageTk.PhotoImage(img3)
+min_calc_canvas.create_image(0, 0, anchor=tk.NW, image = imgtk3)
+min_calc_canvas.create_text(100,10,text="min_frame_calc")
+min_calc_canvas.pack()
+
+root4 = tk.Toplevel(root)
+min_target_canvas = tk.Canvas(root4, width=1920, height=1080)
+min_target_canvas.pack()
+
+img4 = Image.fromarray(min_frame_target)
+imgtk4 = ImageTk.PhotoImage(img4)
+min_target_canvas.create_image(0, 0, anchor=tk.NW, image = imgtk4)
+min_target_canvas.create_text(100,10,text="min_frame_target")
+min_target_canvas.pack()
+
+
+root.update()
+root2.update()
+root3.update()
+root4.update()
+
+
+root.mainloop()
